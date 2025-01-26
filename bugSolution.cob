4f9189da6943217c0e2eaeb5cef1a0539fd42df7
BugHunter-X <askzenai@gmail.com>
2025-01-26 22:28:06 +0100

    Add bugSolution.cob file

---
+++ bugSolution.cob
@@ -0,0 +1,14 @@
+01  WS-AREA-1 PIC X(200). 
+01  WS-AREA-2 PIC X(200).
+01  WS-LENGTH PIC 9(4) COMP.
+
+PROCEDURE DIVISION.
+    MOVE 'This is a much longer string than before' TO WS-AREA-1
+    INSPECT WS-AREA-1 TALLYING WS-LENGTH FOR CHARACTERS
+    IF WS-LENGTH > 200 THEN
+        DISPLAY "Error: String too long!" 
+        STOP RUN
+    END-IF
+    MOVE WS-AREA-1 TO WS-AREA-2
+    DISPLAY WS-AREA-2
+    STOP RUN.
